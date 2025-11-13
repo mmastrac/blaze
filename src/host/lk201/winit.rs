f09c579f4febdba6a5687b29b46e51cdd93bc2d2
Matt Mastracci <matthew@mastracci.com>
2025-11-13 14:48:58 -0700

    Apply key fixes from @j4james (fixes #6)

--- src/host/lk201/winit.rs
+++ src/host/lk201/winit.rs
@@ -10,12 +10,51 @@ pub fn update_keyboard(input: &WinitInputHelper, sender: &LK201Sender) {
         (KeyCode::F3, SpecialKey::F3),
         (KeyCode::F4, SpecialKey::F4),
         (KeyCode::F5, SpecialKey::F5),
+        (KeyCode::F6, SpecialKey::F6),
+        (KeyCode::F7, SpecialKey::F7),
+        (KeyCode::F8, SpecialKey::F8),
+        (KeyCode::F9, SpecialKey::F9),
+        (KeyCode::F10, SpecialKey::F10),
+        (KeyCode::F11, SpecialKey::F11),
+        (KeyCode::F12, SpecialKey::F12),
+        (KeyCode::F13, SpecialKey::F13),
+        (KeyCode::F14, SpecialKey::F14),
+        (KeyCode::F15, SpecialKey::Help),
+        (KeyCode::F16, SpecialKey::Menu),
+        (KeyCode::F17, SpecialKey::F17),
+        (KeyCode::F18, SpecialKey::F18),
+        (KeyCode::F19, SpecialKey::F19),
+        (KeyCode::F20, SpecialKey::F20),
         (KeyCode::ArrowUp, SpecialKey::Up),
         (KeyCode::ArrowDown, SpecialKey::Down),
         (KeyCode::ArrowLeft, SpecialKey::Left),
         (KeyCode::ArrowRight, SpecialKey::Right),
         (KeyCode::Enter, SpecialKey::Return),
         (KeyCode::Backspace, SpecialKey::Delete),
+        (KeyCode::Tab, SpecialKey::Tab),
+        (KeyCode::Home, SpecialKey::Find),
+        (KeyCode::End, SpecialKey::Select),
+        (KeyCode::Insert, SpecialKey::InsertHere),
+        (KeyCode::Delete, SpecialKey::Remove),
+        (KeyCode::PageUp, SpecialKey::PrevScreen),
+        (KeyCode::PageDown, SpecialKey::NextScreen),
+        (KeyCode::NumLock, SpecialKey::KpPf1),
+        (KeyCode::NumpadDivide, SpecialKey::KpPf2),
+        (KeyCode::NumpadMultiply, SpecialKey::KpPf3),
+        (KeyCode::NumpadSubtract, SpecialKey::KpPf4),
+        (KeyCode::Numpad0, SpecialKey::Kp0),
+        (KeyCode::Numpad1, SpecialKey::Kp1),
+        (KeyCode::Numpad2, SpecialKey::Kp2),
+        (KeyCode::Numpad3, SpecialKey::Kp3),
+        (KeyCode::Numpad4, SpecialKey::Kp4),
+        (KeyCode::Numpad5, SpecialKey::Kp5),
+        (KeyCode::Numpad6, SpecialKey::Kp6),
+        (KeyCode::Numpad7, SpecialKey::Kp7),
+        (KeyCode::Numpad8, SpecialKey::Kp8),
+        (KeyCode::Numpad9, SpecialKey::Kp9),
+        (KeyCode::NumpadAdd, SpecialKey::KpHyphen),
+        (KeyCode::NumpadDecimal, SpecialKey::KpPeriod),
+        (KeyCode::NumpadEnter, SpecialKey::KpEnter),
     ] {
         if input.key_pressed(key) {
             if input.held_control() {
@@ -24,17 +63,16 @@ pub fn update_keyboard(input: &WinitInputHelper, sender: &LK201Sender) {
                 } else {
                     sender.send_ctrl_special_key(mapping);
                 }
-                sender.send_ctrl_special_key(mapping);
             } else if input.held_shift() {
                 sender.send_shift_special_key(mapping);
             } else {
                 sender.send_special_key(mapping);
             }
+            return;
         }
     }
 
-    br#"""abcdefghijklmnopqrstuvwxyz0123456789.,-_=+:;'"[]{}\|"""#
-        .iter()
+    br#"!"$#%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"#        .iter()
         .for_each(|&c| {
             let s = &[c];
             let s = str::from_utf8(s).unwrap();
